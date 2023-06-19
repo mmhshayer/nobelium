648f88b5261acd2c8245874f5c0d912ffc284cc1
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2023-06-19 18:15:06 +0600

    updated blog description

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Mohammad Mustakim Hassan',
   email: 'mmhshayer@gmail.com',
   link: 'https://thenoblequest.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: 'Shall rest in quiet only when I find',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
