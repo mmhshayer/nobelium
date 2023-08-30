e39919116be16e14299949caf37aab845a1a9846
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2023-08-31 03:03:25 +0600

    Revert "changed search ->🔍in nav"
    This reverts commit 1571497b24fd5852e730e92ffe2355b6b7dc72d2.

--- components/Header.js
+++ components/Header.js
@@ -9,9 +9,9 @@ const NavBar = () => {
   const BLOG = useConfig()
   const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 0, name: '🔍', to: '/search', show: true },
+    { id: 1, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
+    { id: 2, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
   ]
   return (
     <div className="flex-shrink-0">
