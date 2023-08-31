4dcf534e01c03aebc63dab395216c42d074e6de7
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2023-09-01 03:52:27 +0600

    updated white color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
     extend: {
       colors: {
         day: {
-          DEFAULT: config.lightBackground || '#ffffff'
+          DEFAULT: config.lightBackground || '#F9F8F0'
         },
         night: {
           DEFAULT: config.darkBackground || '#111827'
